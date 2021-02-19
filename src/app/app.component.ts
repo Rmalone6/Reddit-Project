af346ff0da0b0660eec5f486a9acec17abb6c5af
Rmalone6 <60504086+Rmalone6@users.noreply.github.com>
2021-02-19 17:12:01 +0000

    Before article push

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean {
+    console.log(`Adding Article Title : ${title.value} and adding Article Link ${title.value}:`);
+    return false;
+}
+}
